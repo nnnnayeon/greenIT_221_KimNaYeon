36242dd9f87dec8366099f07982c98b1288c0962
Administrator <Administrator@322-77>
2022-06-03 11:21:29 +0900

    테스트2

--- Lect_lv9/src/atm/Bank.java
+++ Lect_lv9/src/atm/Bank.java
@@ -5,6 +5,9 @@ public class Bank {
 	public static void main(String[] args) {
 		System.out.println("김나연 테스트");
 		System.out.println("김나연 테스트");
+		System.out.println("김나연 테스트1");
+		System.out.println("김나연 테스트2");
+		System.out.println("김나연 테스트3");
 		
 
 	}
