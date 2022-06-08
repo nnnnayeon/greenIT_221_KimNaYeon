96aa3dbbf689382f8a8833d4c5a3de946eedefda
kelly <kelly@DESKTOP-66H5QM4>
2022-06-08 13:00:20 +0900

    shop1_2

---
+++ Shop1_2/src/shop/Main.java
@@ -0,0 +1,11 @@
+package shop;
+
+public class Main {
+	public static void main(String[] args) {
+		
+		Shop shop = new Shop();
+		shop.mainMenu();
+		
+	}
+
+}
---
+++ Shop1_2/src/shop/Shop.java
@@ -0,0 +1,15 @@
+package shop;
+
+import java.util.Scanner;
+
+public class Shop {
+	private Scanner scan = new Scanner(System.in);
+	
+	ItemManager im = new ItemManager();
+	UserManager um = new UserManager();
+	
+	public void mainMenu() {
+		
+	}
+
+}
